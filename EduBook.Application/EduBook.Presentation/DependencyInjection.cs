6b899cb80b7106b2a5e8e4775c7321a8ff0ef569
Trọng Phan <dev.sytrong@gmail.com>
2024-04-09 17:07:43 +0700

    Config dependency injection

---
+++ EduBook.Application/EduBook.Presentation/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduBook.Presentation
+{
+    /// <summary>
+    /// Functions for create dependency injections
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// This function to add dependency injection for NuGet Package
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddPackage(this IServiceCollection services)
+        {
+            //Session
+            services.AddSession();
+        }
+
+        /// <summary>
+        /// Create dependencies for service (interface) & service (class) or repository (interface) & repository (class)
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddMasterServices(this IServiceCollection services)
+        {
+            //Example code:
+            //services.AddScoped<IUserService, UserService>();
+            //services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
--- EduBook.Application/EduBook.Presentation/Program.cs
+++ EduBook.Application/EduBook.Presentation/Program.cs
@@ -1,8 +1,10 @@
+using EduBook.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddMasterServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
