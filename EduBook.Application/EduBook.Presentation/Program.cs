28ab0b8cc698240b240a7c4d057dbe52067796be
NhatTiennn <phamtannhat@gmail.com>
2024-04-10 07:35:43 +0700

    Feature-AddPackage-DI

--- EduBook.Application/EduBook.Presentation/Program.cs
+++ EduBook.Application/EduBook.Presentation/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddPackage();
 builder.Services.AddMasterServices();
 var app = builder.Build();
 
