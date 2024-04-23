c4177df1545a7ec71b3e3a08911fd77c690815cc
Trọng Phan <dev.sytrong@gmail.com>
2024-04-23 22:52:12 +0700

    Add room code base

---
+++ EduBook.Application/EduBook.Presentation/Pages/Rooms/DetailsRoom.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EduBook.BusinessObject;
+
+namespace EduBook.Presentation.Pages.Rooms
+{
+    public class DetailsRoomModel : PageModel
+    {
+        private readonly EduBook.BusinessObject.EduBookContext _context;
+
+        public DetailsRoomModel()
+        {
+            _context = new EduBookContext();
+        }
+
+        public Room Room { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Room = room;
+            }
+            return Page();
+        }
+    }
+}
---
+++ EduBook.Application/EduBook.Presentation/Pages/Rooms/EditRoom.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduBook.BusinessObject;
+
+namespace EduBook.Presentation.Pages.Rooms
+{
+    public class EditRoomModel : PageModel
+    {
+        private readonly EduBook.BusinessObject.EduBookContext _context;
+
+        public EditRoomModel()
+        {
+            _context = new EduBookContext();
+        }
+
+        [BindProperty]
+        public Room Room { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room =  await _context.Rooms.FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            Room = room;
+           ViewData["DepartmentId"] = new SelectList(_context.Departments, "DepartmentId", "Address");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Room).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomExists(Room.RoomId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool RoomExists(int id)
+        {
+          return (_context.Rooms?.Any(e => e.RoomId == id)).GetValueOrDefault();
+        }
+    }
+}
