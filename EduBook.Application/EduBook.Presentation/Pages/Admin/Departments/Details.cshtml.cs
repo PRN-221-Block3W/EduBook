7f8c368cd61b4ffa5553592762b2e02b00b34116
Trọng Phan <dev.sytrong@gmail.com>
2024-04-25 04:22:16 +0700

    add base code department, room

---
+++ EduBook.Application/EduBook.Presentation/Pages/Admin/Departments/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EduBook.BusinessObject;
+
+namespace EduBook.Presentation.Pages.Admin.Departments
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly EduBook.BusinessObject.EduBookContext _context;
+
+        public DetailsModel(EduBook.BusinessObject.EduBookContext context)
+        {
+            _context = context;
+        }
+
+      public Department Department { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Departments == null)
+            {
+                return NotFound();
+            }
+
+            var department = await _context.Departments.FirstOrDefaultAsync(m => m.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Department = department;
+            }
+            return Page();
+        }
+    }
+}
---
+++ EduBook.Application/EduBook.Presentation/Pages/Admin/Rooms/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EduBook.BusinessObject;
+
+namespace EduBook.Presentation.Pages.Admin.Rooms
+{
+    public class IndexModel : PageModel
+    {
+        private readonly EduBook.BusinessObject.EduBookContext _context;
+
+        public IndexModel(EduBook.BusinessObject.EduBookContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Room> Room { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Rooms != null)
+            {
+                Room = await _context.Rooms
+                .Include(r => r.Department).ToListAsync();
+            }
+        }
+    }
+}
