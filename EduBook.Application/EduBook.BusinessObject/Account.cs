27009138487e3e8be1b9160267fabd982269ec82
tien-lai-dev <phamtannhat0@gmail.com>
2024-04-25 03:48:08 +0700

    config

--- EduBook.Application/EduBook.BusinessObject/Account.cs
+++ EduBook.Application/EduBook.BusinessObject/Account.cs
@@ -14,7 +14,6 @@ namespace EduBook.BusinessObject
         public int AccountId { get; set; }
         public int RoleId { get; set; }
         public string? UserName { get; set; }
-        public int? DepartmentId { get; set; }
         public string? Phone { get; set; }
         public string? Address { get; set; }
         public DateTime? Dob { get; set; }
@@ -22,7 +21,6 @@ namespace EduBook.BusinessObject
         public string Password { get; set; } = null!;
         public bool Status { get; set; }
 
-        public virtual Department Department { get; set; } = null!;
         public virtual Role Role { get; set; } = null!;
         public virtual ICollection<BookingDetail> BookingDetails { get; set; }
         public virtual ICollection<Comment> Comments { get; set; }
--- EduBook.Application/EduBook.BusinessObject/Room.cs
+++ EduBook.Application/EduBook.BusinessObject/Room.cs
@@ -13,14 +13,13 @@ namespace EduBook.BusinessObject
         }
 
         public int RoomId { get; set; }
-        public int DepartmentId { get; set; }
         public string RoomName { get; set; } = null!;
         public string Description { get; set; } = null!;
         public double LengthRoom { get; set; }
         public double WidthRoom { get; set; }
         public bool Status { get; set; }
+        public string? ImageRoom { get; set; }
 
-        public virtual Department Department { get; set; } = null!;
         public virtual ICollection<Comment> Comments { get; set; }
         public virtual ICollection<RoomEquipment> RoomEquipments { get; set; }
         public virtual ICollection<Slot> Slots { get; set; }
