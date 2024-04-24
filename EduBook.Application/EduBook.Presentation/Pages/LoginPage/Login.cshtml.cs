1d598a13899253221ab67698d797bbf36f642232
nguyencongthinh1210 <87653267+nguyencongthinh1210@users.noreply.github.com>
2024-04-24 15:46:47 +0700

    Login

---
+++ EduBook.Application/EduBook.Presentation/Pages/LoginPage/Login.cshtml.cs
@@ -0,0 +1,48 @@
+using EduBook.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduBook.Presentation.Pages.LoginPage
+{
+	public class LoginModel : PageModel
+	{
+		[BindProperty]
+		[EmailAddress]
+		[Required]
+		public string Email { get; set; }
+		[BindProperty]
+		[Required]
+		public string Password { get; set; }
+		private readonly IAccountService accountService;
+		//private readonly IRoleService roleRepository;
+		public LoginModel(IAccountService accountService)
+		{
+			this.accountService = accountService;
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			var account = accountService.Login(Email, Password);
+			if (account != null)
+			{
+				HttpContext.Session.SetString("Email", Email);
+				HttpContext.Session.SetInt32("AccountId", account.AccountId);
+			}
+			var role = accountService.GetRoles().FirstOrDefault(p => p.RoleId == account.RoleId);
+			if (role.RoleName.ToLower().Equals("customer"))
+			{
+				return Redirect("/Customer/CustomerHomePage");
+			}
+			if (role.RoleName.ToLower().Equals("staff"))
+			{
+				return Redirect("/Staff/StaffHome");
+			}
+			return Page();
+		}
+	}
+}
