07ea01e041dcb7f1c432c7cc425699dbf84d1bc1
Nguyễn Công Thịnh <thinh111190@gmail.com>
2024-04-25 21:15:37 +0700

    FixHistoryBooking

--- EduBook.Application/EduBook.BusinessObject/EduBookContext.cs
+++ EduBook.Application/EduBook.BusinessObject/EduBookContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace EduBook.BusinessObject
 {
@@ -32,9 +33,18 @@ namespace EduBook.BusinessObject
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);Database=EduBook;Uid=sa;Pwd=12345;");
+                optionsBuilder.UseSqlServer(GetConnectionString());
             }
+        }
+
+        private string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var strConn = config["ConnectionStrings:ConnectionStrings"];
+            return strConn;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -180,10 +190,6 @@ namespace EduBook.BusinessObject
                 entity.Property(e => e.DepartmentId).HasColumnName("DepartmentID");
 
                 entity.Property(e => e.Description).HasMaxLength(50);
-
-                entity.Property(e => e.ImageRoom)
-                    .HasMaxLength(255)
-                    .IsUnicode(false);
 
                 entity.Property(e => e.RoomName).HasMaxLength(50);
 
--- EduBook.Application/EduBook.Presentation/Pages/Customer/BookingHistory.cshtml.cs
+++ EduBook.Application/EduBook.Presentation/Pages/Customer/BookingHistory.cshtml.cs
@@ -35,25 +35,19 @@ namespace EduBook.Presentation.Pages.Customer
             Booking = bookingRepository.GetBookingsByAccount((int) accountId);
             return Page();
         }
-    //    public async Task<IActionResult> OnPostAsync(int? id)
-    //    {
-    //        var check = await bookingRepository.GetBookingByBookingId(id.Value);
+        public IActionResult OnPostAsync(int? id)
+        {
+            var check = bookingRepository.GetById(id.Value);
 
-    //        if (check == null)
-    //        {
-    //            return NotFound();
-    //        }
-    //        var booking = bookingRepository.GetBookingId(id.Value);
-    //        booking.Status = false;
-    //        bookingRepository.Update(booking);
-    //        return RedirectToPage("/Customer/BookingHistory");
-    //    }
-
-    //    public async Task<IActionResult> OnPostLogoutAsync()
-    //    {
-    //        httpContextAccessor.HttpContext.Session.Remove("AccountId");
-    //        return Redirect("/Customer/HomePage");
-    //    }
-    //}
+            if (check == null)
+            {
+                return NotFound();
+            }
+            var booking = bookingRepository.GetById(id.Value);
+            booking.Status = false;
+            bookingRepository.Update(booking);
+            return RedirectToPage("/Customer/BookingHistory");
+        }
     }
 }
+
