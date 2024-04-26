45af215531546a677d327e2b1233fa65ffc73407
Nguyễn Công Thịnh <thinh111190@gmail.com>
2024-04-26 07:29:12 +0700

    FixMyProfile
    Thinh-code

---
+++ EduBook.Application/EduBook.Presentation/Pages/Customer/MyProfile.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using EduBook.BusinessObject;
+using EduBook.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EduBook.Presentation.Pages.Customer
+{
+    public class MyProfileModel : PageModel
+    {
+        private readonly IAccountRepository accountRepository;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public MyProfileModel(IAccountRepository accountRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            this.accountRepository = accountRepository;
+            this.httpContextAccessor = httpContextAccessor;
+            Account = new Account();
+        }
+
+        [BindProperty]
+        public Account Account { get; set; } = default!;
+        [BindProperty]
+        public string ConfirmPassword { get; set; }
+
+        public IActionResult OnGetAsync(int? id)
+        {
+            Account = accountRepository.GetById((int)id);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return Page();
+            }
+
+            if (Account.Password != ConfirmPassword)
+            {
+                ModelState.AddModelError(string.Empty, "Password and confirmation do not match.");
+                return Page();
+            }
+
+            var accountToUpdate = accountRepository.GetById((int)id);
+            if (accountToUpdate == null)
+            {
+                return Page();
+            }
+
+            // Cập nhật thông tin tài khoản
+            //accountToUpdate.AccountId = Account.AccountId;
+            accountToUpdate.Password = Account.Password;
+            accountToUpdate.UserName = Account.UserName;
+            accountToUpdate.Email = Account.Email;
+            accountToUpdate.Phone = Account.Phone;
+            accountToUpdate.Email = Account.Email;
+            accountToUpdate.Dob = Account.Dob;
+            accountToUpdate.Status = true;
+
+            accountRepository.Update(accountToUpdate);
+
+            return Redirect("/Customer/CustomerHomePage");
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            httpContextAccessor.HttpContext.Session.Remove("AccountId");
+            return Redirect("/Customer/CustomerHomePage");
+        }
+    }
+}
